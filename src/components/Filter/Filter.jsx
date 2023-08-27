d0f6a7aebf6d7b7c645f7c4eea558d87f3c17e07
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2023-08-27 17:27:17 +0300

    Add components

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,20 @@
+import propTypes from 'prop-types';
+import css from './Filter.module.css';
+
+export const Filter = ({ filter, handleChange }) => (
+  <div>
+    <label
+      className={css.filterName}
+      type="text"
+      name="filter"
+      placeholder="Enter filter"
+      value={filter}
+      onChange={handleChange}
+    />
+  </div>
+);
+
+Filter.propTypes = {
+  filter: propTypes.string.isRequired,
+  handleChange: propTypes.func.isRequired,
+};
